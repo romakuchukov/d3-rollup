4243804be259e07dcfcf497a63453c8b31dfa397
Roma K <romankuchukov@gmail.com>
2020-09-14 17:20:55 -0400

    indentation fixes

--- src/custom.js
+++ src/custom.js
@@ -5,20 +5,20 @@ const width = 1000;
 const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
 const x = d3
-          .scaleLinear()
-          .domain(d3.extent(data, d => d.year))
-          .range([margin.left, width - margin.right]);
+  .scaleLinear()
+  .domain(d3.extent(data, d => d.year))
+  .range([margin.left, width - margin.right]);
 
 const y = d3
-          .scaleLinear()
-          .domain([0, d3.max(data, d => d.value+1)])
-          .range([height - margin.bottom, margin.top]);
+  .scaleLinear()
+  .domain([0, d3.max(data, d => d.value+1)])
+  .range([height - margin.bottom, margin.top]);
 
 const line = d3.line()
-              .curve(d3.curveBasis)
-              .defined(d => !isNaN(d.value))
-              .x(d => x(d.year))
-              .y(d => y(d.value));
+  .curve(d3.curveBasis)
+  .defined(d => !isNaN(d.value))
+  .x(d => x(d.year))
+  .y(d => y(d.value));
 
 const bisect = mx => {
   const year = x.invert(mx);
