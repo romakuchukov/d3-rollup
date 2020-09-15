4a2e431a41577e64658473f3cf698237c7ac1bcd
Roma K <romankuchukov@gmail.com>
2020-09-15 18:49:34 -0400

    small adjustment

--- src/average.js
+++ src/average.js
@@ -7,7 +7,7 @@ export default average = [
   {year:4,value: 2.65},
   {year:5,value: 2.80},
   {year:6,value: 3.00},
-  {year:7,value: 3.20},
+  {year:7,value: 3.10},
   {year:8,value: 3.30},
   {year:9,value: 3.50},
   {year:10,value:3.70}
