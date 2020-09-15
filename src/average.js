da380d33396eb57d4280137bc4bafa52698326c1
Roma K <romankuchukov@gmail.com>
2020-09-14 21:27:59 -0400

    added average

---
+++ src/average.js
@@ -0,0 +1,14 @@
+
+export default average = [
+  {year:0,value: 2.10},
+  {year:1,value: 2.25},
+  {year:2,value: 2.40},
+  {year:3,value: 2.55},
+  {year:4,value: 2.70},
+  {year:5,value: 2.90},
+  {year:6,value: 3.10},
+  {year:7,value: 3.30},
+  {year:8,value: 3.55},
+  {year:9,value: 3.75},
+  {year:10,value:4.00}
+]
